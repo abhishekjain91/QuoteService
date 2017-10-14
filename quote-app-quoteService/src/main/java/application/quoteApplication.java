9c80205948d6668bfd8c1ea78660afca76302e02
Abhishek Jain <abhishek.jain@live.in>
2017-10-14 18:39:08 -0400

    Initial Commit

--- quote-app-quoteService/src/main/java/application/quoteApplication.java
+++ quote-app-quoteService/src/main/java/application/quoteApplication.java
@@ -3,7 +3,9 @@ package application;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class quoteApplication {
 
